baef13e5d55df658ea14a0374d7a55765903faed
Vishal Baghel <112677822+baghel24@users.noreply.github.com>
2024-10-07 15:37:32 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './sections/Header'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <Header/>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/sections/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <p className="text-3xl font-bold underline">
+      Hello world!
+    </p>
+  )
+}
+
+export default Header
